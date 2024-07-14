9b08a75d90a15e8984990a9fe540bb7269f2b4e8
Ayan2002Maity <ayanmaity943@gmail.com>
2024-07-14 20:08:55 +0530

    Day 1 variable and data types

---
+++ Day1/script.js
@@ -0,0 +1,40 @@
+ 
+ //Activity 1- Variable Declaration
+ var v = 12;
+ console.log(v);
+ let l = 'Ayan';
+ console.log(l);
+
+ // Activity 2- constant Declaration
+
+ const c = true;
+ console.log(c);
+
+ // Activity 3- Data Types
+
+ let number = 12;
+ console.log(typeof number);
+ let string = ' Ayan Maity';
+ console.log(typeof string);
+ let boolean = true;
+ console.log(typeof boolean);
+ let array = [1, 2, 3];
+ console.log(typeof array); // object
+ let object = {
+    name : 'Ayan',
+
+ }
+
+ console.log(typeof object); // object
+
+ // Activity 4- Reassigning Variables
+ let a = 12;
+ console.log(a);
+ a = 15;
+ console.log(a);
+
+// Activity 5 - Understansing const
+const b= 12;
+ console.log(b);
+//  b=15;
+//  console.log(b);    // Error: Assignment to constant variable.
